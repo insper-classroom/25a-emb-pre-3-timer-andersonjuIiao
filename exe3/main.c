2a0d2e18b24c5eb566cb866aa55a6d21285d560f
Anderson Julião <antonioaaj@al.insper.edu.br>
2025-03-10 13:30:58 -0300

    try 01 ex.03

--- exe3/main.c
+++ exe3/main.c
@@ -6,6 +6,7 @@ const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
+volatile bool timer_fired = false;
 
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
@@ -16,6 +17,13 @@ void btn_callback(uint gpio, uint32_t events) {
         if (gpio == BTN_PIN_R)
             flag_f_r = 0;
     }
+}
+
+int64_t alarm_callback(alarm_id_t id, void *user_data) {
+    timer_fired = true;
+
+    // Can return a value here in us to fire in the future
+    return 0;
 }
 
 int main() {
@@ -31,9 +39,24 @@ int main() {
     gpio_set_irq_enabled_with_callback(
         BTN_PIN_R, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
 
+    alarm_id_t alarm;
     while (true) {
-
         if (flag_f_r) {
+            alarm = add_alarm_in_ms(500, alarm_callback, NULL, false);    
+            if (!alarm) {
+                printf("Failed to add timer\n");
+            }
+            if(timer_fired){
+                gpio_put(LED_PIN_R,!gpio_get(LED_PIN_R));
+                flag_f_r = 0;
+                timer_fired = false;
+            }
         }
+        if(!flag_f_r&&timer_fired){
+            cancel_alarm(alarm);
+            timer_fired = false;
+
+        }
+
     }
 }
