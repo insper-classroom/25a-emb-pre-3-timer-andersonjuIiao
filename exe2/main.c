7486bb4fbb642e2f4db093364b7a21f60e23a50b
Anderson Julião <antonioaaj@al.insper.edu.br>
2025-03-10 13:24:38 -0300

    try 01 - ex.02

--- exe2/main.c
+++ exe2/main.c
@@ -11,13 +11,26 @@ const int LED_PIN_G = 6;
 volatile int flag_r = 0;
 volatile int flag_g = 0;
 
+volatile int g_timer_red = 0;
+volatile int g_timer_green = 0;
+
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) {
         if (gpio == BTN_PIN_R)
-            flag_r = 1;
+            flag_r = !flag_r;
         else if (gpio == BTN_PIN_G)
-            flag_g = 1;
+            flag_g = !flag_g;
     }
+}
+
+bool timer_red_callback(repeating_timer_t *rt) {
+    g_timer_red = 1;
+    return true; // keep repeating
+}
+
+bool timer_green_callback(repeating_timer_t *rt) {
+    g_timer_green = 1;
+    return true; // keep repeating
 }
 
 int main() {
@@ -39,14 +52,53 @@ int main() {
                                        &btn_callback);
     gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
+    repeating_timer_t timer_red;
+
+    if (!add_repeating_timer_ms(500,
+        timer_red_callback,
+        NULL,
+        &timer_red))
+    {
+    printf("Failed to add timer RED\n");
+    }
+
+    repeating_timer_t timer_green;
+
+    if (!add_repeating_timer_ms(250,
+        timer_green_callback,
+        NULL,
+        &timer_green))
+    {
+    printf("Failed to add timer Green\n");
+    }
+
     while (true) {
 
         if (flag_r) {
-            flag_r = 0;
+
+            if(g_timer_red){
+                gpio_put(LED_PIN_R,!gpio_get(LED_PIN_R));
+                g_timer_red = 0;
+            }
+        }
+        else if(!flag_r){
+            gpio_put(LED_PIN_R,0);
         }
 
         if (flag_g) {
             flag_g = 0;
         }
+
+        if (flag_g) {
+
+            if(g_timer_green){
+                gpio_put(LED_PIN_G,!gpio_get(LED_PIN_G));
+                g_timer_green = 0;
+            }
+        }
+        else if(!flag_g){
+            gpio_put(LED_PIN_R,0);
+        }
+    
     }
 }
