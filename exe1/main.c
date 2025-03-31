8ecca140f898b222f47a802f8331e2dffa382040
Anderson Julião <antonioaaj@al.insper.edu.br>
2025-03-31 10:11:17 -0300

    Estudos PI

--- exe1/main.c
+++ exe1/main.c
@@ -6,19 +6,12 @@ const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
-volatile int g_timer_0 = 0;
-
 
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
-        flag_f_r = !flag_f_r;
+        flag_f_r = 1;
     } else if (events == 0x8) { // rise edge
     }
-}
-
-bool timer_0_callback(repeating_timer_t *rt) {
-    g_timer_0 = 1;
-    return true; // keep repeating
 }
 
 int main() {
@@ -31,29 +24,12 @@ int main() {
     gpio_pull_up(BTN_PIN_R);
 
     gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                       &btn_callback);
-
-    repeating_timer_t timer_0;
-
-    if (!add_repeating_timer_us(500, 
-        timer_0_callback,
-        NULL, 
-        &timer_0)) 
-    {
-        printf("Failed to add timer\n");
-    }
+                                    &btn_callback);
 
     while (true) {
 
         if (flag_f_r) {
-
-            if(g_timer_0){
-                gpio_put(LED_PIN_R,!gpio_get(LED_PIN_R));
-                g_timer_0 = 0;
-            }
-        }
-        if(!flag_f_r){
-            gpio_put(LED_PIN_R,0);
+            flag_f_r = 0;
         }
     }
 }
--- exe2/main.c
+++ exe2/main.c
@@ -11,26 +11,13 @@ const int LED_PIN_G = 6;
 volatile int flag_r = 0;
 volatile int flag_g = 0;
 
-volatile int g_timer_red = 0;
-volatile int g_timer_green = 0;
-
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) {
         if (gpio == BTN_PIN_R)
-            flag_r = !flag_r;
+            flag_r = 1;
         else if (gpio == BTN_PIN_G)
-            flag_g = !flag_g;
+            flag_g = 1;
     }
-}
-
-bool timer_red_callback(repeating_timer_t *rt) {
-    g_timer_red = 1;
-    return true; // keep repeating
-}
-
-bool timer_green_callback(repeating_timer_t *rt) {
-    g_timer_green = 1;
-    return true; // keep repeating
 }
 
 int main() {
@@ -49,52 +36,17 @@ int main() {
     gpio_pull_up(BTN_PIN_G);
 
     gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                       &btn_callback);
+                                    &btn_callback);
     gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
-
-    repeating_timer_t timer_red;
-
-    if (!add_repeating_timer_ms(500,
-        timer_red_callback,
-        NULL,
-        &timer_red))
-    {
-    printf("Failed to add timer RED\n");
-    }
-
-    repeating_timer_t timer_green;
-
-    if (!add_repeating_timer_ms(250,
-        timer_green_callback,
-        NULL,
-        &timer_green))
-    {
-    printf("Failed to add timer Green\n");
-    }
 
     while (true) {
 
         if (flag_r) {
-
-            if(g_timer_red){
-                gpio_put(LED_PIN_R,!gpio_get(LED_PIN_R));
-                g_timer_red = 0;
-            }
-        }
-        else if(!flag_r){
-            gpio_put(LED_PIN_R,0);
+            flag_r = 0;
         }
 
         if (flag_g) {
-
-            if(g_timer_green){
-                gpio_put(LED_PIN_G,!gpio_get(LED_PIN_G));
-                g_timer_green = 0;
-            }
+            flag_g = 0;
         }
-        else if(!flag_g){
-            gpio_put(LED_PIN_G,0);
-        }
-    
     }
 }
